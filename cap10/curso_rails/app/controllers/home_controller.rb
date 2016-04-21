437806cf9c4c6a5e4eb62be35bcca8ce12ed7413
Santiago Medina <devsanlizmed@yahoo.com>
2016-04-21 17:20:04 -0500

    Links example added

--- cap10/curso_rails/app/controllers/home_controller.rb
+++ cap10/curso_rails/app/controllers/home_controller.rb
@@ -1,13 +1,26 @@
 class HomeController < ApplicationController
+  # Use application layout
+  layout "application"
   # Use application layout with exception
-  layout "application", :except => :contacto
+  #layout "application", :except => :contacto
   
   def index
+    @title = "Index"
   end
 
   def quienes_somos
+    @title = "Quiénes somos"
   end
 
   def contacto
+    @title = "Contacto"
+  end
+  
+  def localizacion
+    @title = "Localización"
+  end
+  
+  def servicios
+    @title = "Servicios"
   end
 end
